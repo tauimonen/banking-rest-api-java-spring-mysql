215833ecd95ee73a19ce8e7b373074631b9f0d9e
vgtoui <toni.uimonen@tuni.fi>
2024-02-08 09:05:13 +0200

    Create a Spring Boot project

---
+++ src/main/java/dev/tau/bankingbackend/BankingBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.tau.bankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tau/bankingbackend/BankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tau.bankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
