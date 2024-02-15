92adc3b4a9f06cde182eaba0ac398dd70c30012a
vgtoui <toni.uimonen@tuni.fi>
2024-02-15 11:50:52 +0200

    Add RepositoryRestResource annotations to repositorios

--- src/main/java/dev/tau/bankingbackend/repository/CustomerRepository.java
+++ src/main/java/dev/tau/bankingbackend/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package dev.tau.bankingbackend.repository;
 
 import dev.tau.bankingbackend.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="customer")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
--- src/main/java/dev/tau/bankingbackend/repository/TransactionRepository.java
+++ src/main/java/dev/tau/bankingbackend/repository/TransactionRepository.java
@@ -2,9 +2,11 @@ package dev.tau.bankingbackend.repository;
 
 import dev.tau.bankingbackend.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path="transaction")
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByCustomerId(Long customerId);
 }
